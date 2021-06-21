a082993b29beae99ed79ee5c8c0a97a8bc766ce0
Paulosix12 <paulo_edu_12@hotmail.com>
2021-06-21 20:56:07 -0300

    Liftoff - Aula 1

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import './App.css';
+import { Button } from './Components/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button></Button>
+    </div>
+  );
+}
+
+export default App;
+ 
